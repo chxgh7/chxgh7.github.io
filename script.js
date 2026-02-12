6a868e473c0202434a7c4b635c48bf71c0938228
chxgh7 <chrxj8@gmail.com>
2026-02-12 22:54:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-document.querySelectorAll("nav a").forEach(anchor => {
-    anchor.addEventListener("click", function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute("href"))
-            .scrollIntoView({ behavior: "smooth" });
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+        }
     });
+});
+
+sections.forEach(section => {
+    observer.observe(section);
 });
